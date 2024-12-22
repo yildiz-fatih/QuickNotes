2c246931b4761b689e8f2229658a1a5d05ee6caa
Fatih <fa.yildiz99@gmail.com>
2024-12-22 07:34:51 +0300

    refactored the register request dto, removed unnecessary field(s)

--- QuickNotes.Business/DTOs/Account/RegisterRequest.cs
+++ QuickNotes.Business/DTOs/Account/RegisterRequest.cs
@@ -6,5 +6,4 @@ public class RegisterRequest
     public string UserName { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
-    public string RoleSelected { get; set; }
 }
